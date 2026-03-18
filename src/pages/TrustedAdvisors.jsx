3b45df1e792b16a00fdfb308ac74b50b0e094490
David Tamale-Sali <david@theexitindex.com>
2026-03-18 23:42:02 +0000

    Render trusted advisors page natively

--- src/pages/TrustedAdvisors.jsx
+++ src/pages/TrustedAdvisors.jsx
@@ -1,8 +1,17 @@
-import { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function TrustedAdvisors() {
+  const [html, setHtml] = useState('')
+
   useEffect(() => {
-    window.location.href = '/trusted-advisors-page.html'
+    fetch('/trusted-advisors-page.html')
+      .then(r => r.text())
+      .then(text => {
+        document.open()
+        document.write(text)
+        document.close()
+      })
   }, [])
+
   return null
 }
